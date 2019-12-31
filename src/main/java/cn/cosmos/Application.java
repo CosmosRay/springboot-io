1861173549bdf57458e18781a40b92801d62628a
leiyuzhou <leiyuzhou@topnet.net.cn>
2019-12-31 15:38:02 +0800

    控制文件上传大小

--- src/main/java/cn/cosmos/Application.java
+++ src/main/java/cn/cosmos/Application.java
@@ -2,7 +2,11 @@ package cn.cosmos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * Created with CosmosRay
@@ -18,4 +22,18 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
+
+    /**
+     * 方法二：启动文件配置 文件上传配置
+     * @return
+     */
+    /*@Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大 5M    //KB,MB
+        factory.setMaxFileSize("5120KB");
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }*/
 }
